eb1769ea9ccf37c094fdcf90f8e7e9f62fc78462
MasenBoyce <100795006+MasenBoyce@users.noreply.github.com>
2022-04-19 09:15:21 -0600

    Finalizing chapter 8

--- Chapter8/src/Bank/BusinessAcct.java
+++ Chapter8/src/Bank/BusinessAcct.java
@@ -1,19 +1,31 @@
+/*
+ 
+	Program: BusinessAcct.java 				Last Date of this Revision: April 9, 2022 
+	Purpose: This class stores the users information if they choose a business class and returns the account balance.
+	Author: Masen Boyce
+	School: CHHS
+	Course: Computer Science 30
+	 
+*/
+
 package Bank;
 public class BusinessAcct extends Account 
 {
-	private double minBal = 500;
+	private double Bal;
 	
 	
 	
-	public BusinessAcct(double bal, String fName, String lName, String str, String city, String st, String zip) 
+	public BusinessAcct(double bal, String fName, String lName, String str, String city, String st, String zip) //constructor
 	{
 		super(bal, fName, lName, str, city, st, zip);
 		
-		
-		
-		if (bal < minBal) 
-		{
-			bal = bal - 10;
-		}
+		Bal = bal;
+	}
+	
+	
+	
+	public String toString() //returns data of the constructor
+	{
+		return("Balance: $" + Bal);
 	}
 }
