5fdb11118be38202672a1efd7497b1839c2ca313
MasenBoyce <100795006+MasenBoyce@users.noreply.github.com>
2022-04-19 09:24:43 -0600

    Finalizing Chapter 8

--- Chapter8/src/Bank/PersonalAcct.java
+++ Chapter8/src/Bank/PersonalAcct.java
@@ -1,3 +1,13 @@
+/*
+ 
+	Program: PersonalAcct.java 				Last Date of this Revision: April 9, 2022 
+	Purpose: This class stores the users information if the choose a personal account and returns the balance of the account.
+	Author: Masen Boyce
+	School: CHHS
+	Course: Computer Science 30
+	 
+*/
+
 package Bank;
 public class PersonalAcct extends Account 
 {
@@ -5,9 +15,9 @@ public class PersonalAcct extends Account
 	
 	
 	
-	public PersonalAcct(double bal, String fName, String lName, String str, String city, String st, String zip) 
+	public PersonalAcct(double bal, String fName, String lName, String str, String city, String st, String zip) //allows access to these variables from o
 	{
-		super(bal, fName, lName, str, city, st, zip);
+		super(bal, fName, lName, str, city, st, zip); //An account has been created. Balance and customer data has been initialized with parameters.
 		
 		Bal = bal;
 	}
@@ -19,4 +29,3 @@ public class PersonalAcct extends Account
 		return("Balance: $" + Bal);
 	}
 }
-
